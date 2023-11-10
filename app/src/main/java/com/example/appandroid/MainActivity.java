ba141c554e4258bc96605b17e8fb902535705b49
Leonardo Santana <leonardosantana2203@gmail.com>
2023-11-10 09:39:04 -0500

    first commit

---
+++ app/src/main/java/com/example/appandroid/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.appandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.appandroid.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+    }
+}
